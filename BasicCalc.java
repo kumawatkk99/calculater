77a66378b6663cbe6750232fb7bc3564e08d73b3
Kushal Kumawat <kuskumaw@publicisgroupe.net>
2022-01-23 09:44:39 +0530

    fc

---
+++ BasicCalc.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+public class SimpleCalc {
+	public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+
+	System.out.println("Enter First Number");
+	Double n1=sc.nextDouble();
+	System.out.println("Enter Second Number");
+	Double n2=sc.nextDouble();
+	
+
+	System.out.println(" Please Select the operator : +,-,*, or /");
+	char op=sc.next().charAt(0);
+	 
+	switch(op) {
+		case '+':
+			System.out.println("Addition of numbers is "+(n1+n2));
+			break;
+		case '-':
+			System.out.println("Subtraction of numbers is "+(n1-n2));
+			break;
+	
+		case '*':
+			System.out.println("Multiplication of numbers is "+(n1*n2));
+			break;
+		case '/':
+			
+			if(n2==0) { 
+				System.out.println("Please Enter Valid Denominator Value");
+				break;
+			}
+			else {
+			System.out.println("Division of numbers is "+(n1/n2));
+			break;
+			}
+	}
+	sc.close();
+	
+	}
+}
